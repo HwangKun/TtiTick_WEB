613484e4347c4540d5dc5217e6d9b3e4c06a0620
HwangKun <clabstar@naver.com>
2015-04-19 01:03:49 +0900

    세번째 테스트커밋, 스마티 추가

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<html>
-<body>
-테스트
-<body>
-</html>
+<?php
+require 'smarty/libs/Smarty.class.php';
+
+$smarty = new Smarty();
+
+$smarty->template_dir = 'themes';
+
+$smarty->assign('name', 'Ned');
+$smarty->display('index.tpl');
+?>
---
+++ templates_c/cf7c8410593250cfc1a6eca12ef44a40f0bcc1dd.file.index.tpl.php
@@ -0,0 +1,34 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-19 01:00:45
+         compiled from "themes\index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2658855327fadd84a90-16890415%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'cf7c8410593250cfc1a6eca12ef44a40f0bcc1dd' => 
+    array (
+      0 => 'themes\\index.tpl',
+      1 => 1429372684,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2658855327fadd84a90-16890415',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'name' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_55327fae607e72_45254508',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_55327fae607e72_45254508')) {function content_55327fae607e72_45254508($_smarty_tpl) {?><html>
+<head>
+    <title>Smarty</title>
+</head>
+<body>
+Hello, <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+!
+</body>
+</html><?php }} ?>
